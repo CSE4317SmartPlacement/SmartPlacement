db9a35e00b1744679e58e53b29b1c9edd9820f1c
michael.stokes <cse3311.team3@gmail.com>
2022-08-05 20:39:52 -0500

    Merge error?

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require("path");
 const cors = require("cors");
 const PORT = process.env.PORT || 8080;
-
+//test
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname)+"/client/build"));
